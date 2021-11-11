7744b873ea7057d279b16cd115c9dcc4606d79f2
liviaalcantaraf <liviaalcantaraf@gmail.com>
2021-11-11 11:23:43 +0000

    Added personalized icon

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import react from "react";
 import FormattedDate from "./FortmattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -14,11 +15,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-5">
         <div className="col-6">
           <div className="clear-fix">
-            <img
-              src={props.data.icon}
-              alt={props.data.description}
-              className="flot-left"
-            />
+            <WeatherIcon code={props.data.icon} />
             <span className="temperature">
               {Math.round(props.data.temperature)}
             </span>{" "}
