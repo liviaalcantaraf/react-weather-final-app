2d995a6b5f826c706f9a05dd32060d14e4ca2ee3
liviaalcantaraf <liviaalcantaraf@gmail.com>
2021-11-14 12:37:11 +0000

    git hub link

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <Weather defaultCity="Lisbon" />
 
       <footer>
-        Coded by <a href="#">Lívia A.</a> Check out the{" "}
+        Coded by{" "}
+        <a href="https://github.com/liviaalcantaraf/react-weather-final-app">
+          Lívia A.
+        </a>{" "}
+        Check out the{" "}
         <a href="https://www.linkedin.com/in/liviaalcantaraf/">
           open-sourced code
         </a>
