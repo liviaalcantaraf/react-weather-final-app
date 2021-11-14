97d6de2566ff91a8c13a698f1f8990726fcca3f3
liviaalcantaraf <liviaalcantaraf@gmail.com>
2021-11-14 21:59:43 +0000

    added firts forecast

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -16,7 +16,7 @@ export default function WeatherInfo(props) {
       <div className="row mt-5">
         <div className="col-6">
           <div className="clear-fix">
-            <WeatherIcon code={props.data.icon} />
+            <WeatherIcon code={props.data.icon} size={60} />
           </div>
           <div className="float-left">
             <WeatherTemperature celsius={props.data.temperature} />
