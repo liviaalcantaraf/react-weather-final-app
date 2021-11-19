882b8c93bbf3d43c896ddda7b40217dd5c5c9aa3
liviaalcantaraf <liviaalcantaraf@gmail.com>
2021-11-19 14:00:49 +0000

    aply the correct api url

--- src/Weather.js
+++ src/Weather.js
@@ -23,7 +23,7 @@ export default function Weather(props) {
 
   function search() {
     const apiKey = "eced894402200c20c901ff2b763fcc81";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
   }
