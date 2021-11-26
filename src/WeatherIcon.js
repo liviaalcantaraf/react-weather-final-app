8a509c852f96a24e802d47a556a9f9dae6829854
liviaalcantaraf <liviaalcantaraf@gmail.com>
2021-11-26 17:14:14 +0000

    change icon

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,34 +1,27 @@
 import React from "react";
-import ReactAnimatedWeather from "react-animated-weather";
+import { WiDaySunny } from "weather-icons-react";
 
 export default function WeatherIcon(props) {
   const codeMapping = {
-    "01d": "CLEAR_DAY",
-    "01n": "CLEAR_NIGHT",
-    "02d": " PARTLY_CLOUDY_DAY",
-    "02n": "PARTLY_CLOUDY_NIGHT",
-    "03d": "PARTLY_CLOUDY_DAY",
-    "03n": "PARTLY_CLOUDY_NIGHT",
-    "04d": "CLOUDY",
-    "04n": "CLOUDY",
-    "09d": "RAIN",
-    "09n": "RAIN",
-    "10d": "RAIN",
-    "10n": "RAIN",
-    "11d": "RAIN",
-    "11n": "RAIN",
-    "13d": "SNOW",
-    "13n": "SNOW",
-    "50d": "FOG",
-    "50n": "FOG",
+    "01d": "WiDaySunny",
+    "01n": "WiNightClear",
+    "02d": "WiDayCloudy",
+    "02n": "WiNightAltCloudy",
+    "03d": "WiCloud",
+    "03n": "WiNightCloudy",
+    "04d": "WiCloudy",
+    "04n": "WiCloudy",
+    "09d": "WiDayShowers",
+    "09n": "WiNightShowers",
+    "10d": "WiDayRainWind",
+    "10n": "WiNightAltRainWind",
+    "11d": "WiDayThunderstorm",
+    "11n": "WiNightThunderstorm",
+    "13d": "WiSnowflakeCold",
+    "13n": "WiSnowflakeCold",
+    "50d": "WiFog",
+    "50n": "WiFog",
   };
 
-  return (
-    <ReactAnimatedWeather
-      icon={codeMapping[props.code]}
-      color="blue"
-      size={props.size}
-      animate={true}
-    />
-  );
+  return <WiDaySunny size={24} color="#000" />;
 }
--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -2,18 +2,10 @@ import React, { useState } from "react";
 
 export default function WeatherTemperature(props) {
   const [unit, setUnit] = useState("celsius");
-  function convertToFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
 
   function convertToCelsius(event) {
     event.preventDefault();
     setUnit("celsius");
-  }
-
-  function fahrenheit() {
-    return (props.celsius * 9) / 5 + 32;
   }
 
   if (unit === "celsius") {
