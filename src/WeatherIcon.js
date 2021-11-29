2ad1d4f9eb6600e6c5a2d4e415315cc4c9056c0e
liviaalcantaraf <liviaalcantaraf@gmail.com>
2021-11-29 12:47:12 +0000

    changed icons

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,27 +1,43 @@
 import React from "react";
-import { WiDaySunny } from "weather-icons-react";
+import {
+  WiDaySunny,
+  WiNightClear,
+  WiDayCloudy,
+  WiNightAltCloudy,
+  WiCloud,
+  WiNightCloudy,
+  WiCloudy,
+  WiDayShowers,
+  WiNightShowers,
+  WiDayRainWind,
+  WiNightAltRainWind,
+  WiDayThunderstorm,
+  WiNightThunderstorm,
+  WiSnowflakeCold,
+  WiFog,
+} from "weather-icons-react";
 
 export default function WeatherIcon(props) {
   const codeMapping = {
-    "01d": "WiDaySunny",
-    "01n": "WiNightClear",
-    "02d": "WiDayCloudy",
-    "02n": "WiNightAltCloudy",
-    "03d": "WiCloud",
-    "03n": "WiNightCloudy",
-    "04d": "WiCloudy",
-    "04n": "WiCloudy",
-    "09d": "WiDayShowers",
-    "09n": "WiNightShowers",
-    "10d": "WiDayRainWind",
-    "10n": "WiNightAltRainWind",
-    "11d": "WiDayThunderstorm",
-    "11n": "WiNightThunderstorm",
-    "13d": "WiSnowflakeCold",
-    "13n": "WiSnowflakeCold",
-    "50d": "WiFog",
-    "50n": "WiFog",
+    "01d": <WiDaySunny size={24} color="#000" />,
+    "01n": <WiNightClear size={24} color="#000" />,
+    "02d": <WiDayCloudy size={24} color="#000" />,
+    "02n": <WiNightAltCloudy size={24} color="#000" />,
+    "03d": <WiCloud size={24} color="#000" />,
+    "03n": <WiNightCloudy size={24} color="#000" />,
+    "04d": <WiCloudy size={24} color="#000" />,
+    "04n": <WiCloudy size={24} color="#000" />,
+    "09d": <WiDayShowers size={24} color="#000" />,
+    "09n": <WiNightShowers size={24} color="#000" />,
+    "10d": <WiDayRainWind size={24} color="#000" />,
+    "10n": <WiNightAltRainWind size={24} color="#000" />,
+    "11d": <WiDayThunderstorm size={24} color="#000" />,
+    "11n": <WiNightThunderstorm size={24} color="#000" />,
+    "13d": <WiSnowflakeCold size={24} color="#000" />,
+    "13n": <WiSnowflakeCold size={24} color="#000" />,
+    "50d": <WiFog size={24} color="#000" />,
+    "50n": <WiFog size={24} color="#000" />,
   };
 
-  return <WiDaySunny size={24} color="#000" />;
+  return codeMapping[props.code];
 }
